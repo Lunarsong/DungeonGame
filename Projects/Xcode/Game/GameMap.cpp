1016ca23aaeb7b2abc2ae7cb59b71804ac3b43f2
Lunarsong <shanee.nishry@gmail.com>
2013-12-27 10:55:24 +0200

    First push

---
+++ Projects/Xcode/Game/GameMap.cpp
@@ -0,0 +1,114 @@
+//
+//  GameMap.cpp
+//  DungeonGame
+//
+//  Created by Shanee on 12/26/13.
+//
+//
+
+#include "GameMap.h"
+
+GameMap::GameMap()
+{
+    m_pMap = NULL;
+    m_iSizeX = 0;
+    m_iSizeY = 0;
+}
+
+GameMap::~GameMap()
+{
+    Destroy();
+}
+
+void GameMap::Destroy()
+{
+    if ( m_pMap )
+    {
+        delete [] m_pMap;
+        m_pMap = NULL;
+    }
+    
+    m_iSizeX = 0;
+    m_iSizeY = 0;
+}
+
+bool GameMap::Create( const BinaryResource* pResource )
+{
+    Destroy();
+    
+    if ( CreateFromAscii( pResource ) )
+        return true;
+    
+    return false;
+}
+
+bool GameMap::CreateFromAscii( const BinaryResource* pResource )
+{
+    const char* pBuffer = pResource->Buffer();
+    unsigned int uiSize = pResource->BlobSize();
+    
+    bool bSuccess = false;
+    for ( unsigned int i = 0; i < uiSize; ++i )
+    {
+        if ( pBuffer[ i ] == '\n' )
+        {
+            m_iSizeX = i;
+            m_iSizeY = i;
+            
+            bSuccess = true;
+            
+            break;
+        }
+    }
+    
+    if ( !bSuccess )
+    {
+        return false;
+    }
+    
+    bSuccess = false;
+    m_pMap = new char[ m_iSizeY * m_iSizeX ];
+    
+    
+    unsigned int iTilePos = 0;
+    unsigned int iX = 0;
+    for ( unsigned int i = 0; i < uiSize; ++i )
+    {
+        // Reach end of line
+        if ( iX == m_iSizeX )
+        {
+            if ( pBuffer[ i ] != '\n' )
+            {
+                throw "Expected endline";
+            }
+            
+            else
+            {
+                iX = 0;
+                continue;
+            }
+        }
+        
+        if ( pBuffer[ i ] == '\n' )
+            throw "Unexpected newline";
+        
+        ++iX;
+        m_pMap[ iTilePos++ ] = pBuffer[ i ];
+    }
+    
+    return true;;
+}
+
+char& GameMap::operator() ( unsigned int iX, unsigned int iY )
+{
+    assert( iX < m_iSizeX && iY < m_iSizeY && m_pMap );
+    
+    return m_pMap[ m_iSizeX * iY + iX ];
+}
+
+const char& GameMap::operator() ( unsigned int iX, unsigned int iY ) const
+{
+    assert( iX < m_iSizeX && iY < m_iSizeY && m_pMap );
+    
+    return m_pMap[ m_iSizeX * iY + iX ];
+}
---
+++ Source/main.cpp
@@ -0,0 +1,49 @@
+//
+//  main.c
+//  DungeonGame
+//
+//  Created by Shanee on 12/25/13.
+//
+//
+
+#include <stdio.h>
+#include <Engine.h>
+#include <Core/Utils/FileUtils.h>
+#include "GameMap.h"
+#include <Game/Entities/Components/Rendering/TileMapComponent.h>
+
+using namespace Engine;
+
+void Start()
+{
+    FolderAssetBundle* pBundle = new FolderAssetBundle( FileUtils::GetWorkingFolder() );
+    AssetManager::Get().AddAssetBundle( pBundle );
+    
+    BinaryResource* pResource = AssetManager::Get().GetAsset< BinaryResource >( "Map.map" );
+    GameMap map;
+    map.Create( pResource );
+    
+    ITexture* pMapTexture = AssetManager::Get().GetAsset< ITexture >( "Tileset.png" );
+    
+    Entity* pMapEntity = Game::CreateEntity();
+    TileMapComponent* pTileMap = new TileMapComponent( 10, 10, 32.0f, pMapTexture, [&] ( unsigned int x, unsigned int y, RectF& rect )
+                                             {
+                                                 char myChar = map( x, y );
+                                                 myChar = myChar;
+                                             }
+                                             );
+    
+    pMapEntity->AddComponent( pTileMap );
+    pTileMap->Start();
+    
+}
+
+void Update( float fDeltaSeconds )
+{
+    
+}
+
+void End()
+{
+    
+}
